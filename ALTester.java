6c26e1eb170ea1235dac8a8b1023282c21aa16f6
eortiz-30 <eortiz30@stuy.edu>
2021-12-12 16:03:04 -0500

    lab02

--- ALTester.java
+++ ALTester.java
@@ -1,1 +1,32 @@
-foo
+import java.util.ArrayList;
+
+public class ALTester{
+  public static void main(String[] args){
+
+      ArrayList <Integer> isSor = new ArrayList <Integer>();
+      for (int i = 0; i < 23; i++){
+        isSor.add(i);
+      }
+
+      ArrayList <Integer> notSor = new ArrayList <Integer> ();
+      for (int i = 0; i < 23; i++){
+        notSor.add(i);
+      }
+      notSor.set(0, 2);
+
+      System.out.println("Should be false ... " + isSorted(notSor));
+      System.out.println("Should be true ... " + isSorted(isSor));
+  }
+
+  private static boolean isSorted(ArrayList <Integer> a){
+    if (a.size() == 1){
+      return true;
+    }
+    for (int i = 0; i< a.size()-1; i++){
+      if (a.get(i) > a.get(i+1)){
+        return false;
+      }
+    }
+    return true;
+  }
+}
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,1 +1,22 @@
-foo
+import java.util.ArrayList;
+
+public class OrderedArrayList{
+
+  private int findIndex (ArrayList <Integer> a, int value){
+    for (int i = 0; i < a.size(); i++){
+      if (value <= a.get(i)){
+        return i;
+      }
+    }
+    return a.size();
+  }
+
+  private void add(ArrayList <Integer> a, int value, int index){
+    // move numbers to the right
+    for (int i = a.size(); i > index; i--){
+      a.add(i, a.get(i-1));
+    }
+
+    a.add(index, value);
+  }
+}
